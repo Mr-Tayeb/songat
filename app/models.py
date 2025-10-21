e27f3cde39669a3a47b624d98b7075fe595a77ce
Tayeb <abijoue.ta@gmail.com>
2025-10-21 01:56:30 +0100

    move media directory to global project and add directories for users uploads

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to="app/artists/")
+    image = models.ImageField(upload_to="app/artists_images/")
 
     def __str__(self):
         return self.name
@@ -15,7 +15,7 @@ class Album(models.Model):
     title = models.CharField(max_length=200)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums')
     release_date = models.DateField(blank=True, null=True)
-    cover = models.ImageField(upload_to='app/albums/', blank=True, null=True)
+    cover = models.ImageField(upload_to='app/albums_images/', blank=True, null=True)
     
     def __str__(self):
         return f"{self.title} by {self.artist.name}"
