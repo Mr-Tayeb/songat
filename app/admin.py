00bff4d491022c6a7d576ee76361a8f5259771c9
Tayeb <abijoue.ta@gmail.com>
2025-10-21 15:55:16 +0100

    Fix tuple format in admin models

--- app/admin.py
+++ app/admin.py
@@ -4,37 +4,37 @@ from .models import *
 # Register your models here.
 
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ("name")
-    list_filter = ("name")
-    search_fields = ("name", "bio")
+    list_display = ("name",)
+    list_filter = ("name",)
+    search_fields = ("name", "bio",)
     ordering = ('name',)
 
 admin.site.register(Artist, ArtistAdmin)
 
 
 class AlbumAdmin(admin.ModelAdmin):
-    list_display = ("title", "artist", "release_date")
-    list_filter = ("artist", "release_date")
-    search_fields = ("title", "artist", "release_date")
-    ordering = ('artist')
+    list_display = ("title", "artist", "release_date",)
+    list_filter = ("artist", "release_date",)
+    search_fields = ("title", "artist", "release_date",)
+    ordering = ('artist',)
 
 admin.site.register(Album, AlbumAdmin)
 
 
 class SongAdmin(admin.ModelAdmin):
-    list_display = ("title", "artist", "album", "duration", "created_at")
-    list_filter = ("title", "artist", "album", "created_at")
-    search_fields = ("title", "artist", "album")
-    ordering = ('-created_at')
+    list_display = ("title", "artist", "album", "duration", "created_at",)
+    list_filter = ("title", "artist", "album", "created_at",)
+    search_fields = ("title", "artist", "album",)
+    ordering = ('-created_at',)
 
 admin.site.register(Song, SongAdmin)
 
 
 class PlaylistAdmin(admin.ModelAdmin):
-    list_display = ("name", "created_at")
+    list_display = ("name", "created_at",)
     list_filter = ("name", )
-    search_fields = ("name", "songs")
-    ordering = ('-created_at')
+    search_fields = ("name", "songs",)
+    ordering = ('-created_at',)
 
 admin.site.register(Playlist, PlaylistAdmin)
 
