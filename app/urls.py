1964ea9d371b8aca599ebf2db0ed703d02f3763b
Tayeb <abijoue.ta@gmail.com>
2025-10-25 15:08:49 +0100

    Add register/login/logout and profile templates

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
 from . import views
+from .views import *
+
+
 
 urlpatterns = [
     path('', views.home, name='Home'),
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
+    path('profile/', Profile.as_view(), name='profile'),
+    path('register/', Register.as_view(), name='register'),
     path('song/<int:pk>', views.song_details, name='song_details'),
     path('artist/<int:pk>', views.artist_details, name='artist_details'),
     path('album/<int:pk>', views.album_details, name='album_details'),
--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 
@@ -37,3 +45,42 @@ def playlist_details(request, pk):
     context = {"playlist": playlist, "songs": songs}
 
     return render(request, 'app/playlist.html', context)
+
+
+# Authentication views:
+
+class Profile(LoginRequiredMixin, TemplateView):
+    login_url = '/login'
+    template_name = 'app/profile.html'
+
+class Register(FormView):
+    template_name = 'app/register.html'
+    form_class = RegisterForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+
+        return super(Register, self).form_valid(form)
+
+
+class Login(LoginView):
+    template_name = 'app/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        messages.info(self.request, 'Welcome to SONGAT profile')
+        return reverse_lazy('profile')
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid password or username')
+
+        return self.render_to_response(self.get_context_data(form = form))
+
+class Logout(LogoutView):
+    template_name = 'app/logout.html'
+    next_page = reverse_lazy('login')
+    
